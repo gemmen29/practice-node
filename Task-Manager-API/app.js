d9d11429b6eee95d1d37d9b446539077af72be72
George Magdy <georgtriple@gmail.com>
2022-05-29 22:30:25 +0200

    Create basic routes

--- Task-Manager-API/app.js
+++ Task-Manager-API/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const tasksRouter = require('./routes/tasks');
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World');
-});
+// middleware
+app.use(express.json());
+
+app.use('/api/v1/tasks', tasksRouter);
 
 const port = process.env.PORT || 3000;
 
