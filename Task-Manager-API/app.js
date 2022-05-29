6da522e5bbd5e4d33eb771b4dc401d0d86d1ea78
George Magdy <georgtriple@gmail.com>
2022-05-29 21:13:21 +0200

    Basic express setup

--- Task-Manager-API/app.js
+++ Task-Manager-API/app.js
@@ -1,1 +1,12 @@
-console.log('Task-Manager-API');
+const express = require('express');
+const app = express();
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
