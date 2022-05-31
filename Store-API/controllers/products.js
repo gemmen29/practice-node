c2247dfb37ba81ede5df48d8a4ec6389c36b6d21
George Magdy <georgtriple@gmail.com>
2022-05-31 06:50:38 +0200

    Add basic controller implementation and testing route

--- Store-API/controllers/products.js
+++ Store-API/controllers/products.js
@@ -1,5 +1,17 @@
-const getAllProducts = (req, res) => {
-  res.status(200).json({ msg: 'Products route works!' });
+const Product = require('../models/product');
+
+const getAllProductsTesting = async (req, res) => {
+  const products = await Product.find({
+    featured: true,
+  });
+  console.log(products);
+  res.status(200).json({ nbHits: products.length, products });
 };
 
-module.exports = { getAllProducts };
+const getAllProducts = async (req, res) => {
+  const products = await Product.find({});
+  console.log(products);
+  res.status(200).json({ nbHits: products.length, products });
+};
+
+module.exports = { getAllProductsTesting, getAllProducts };
