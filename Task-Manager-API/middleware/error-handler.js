80cb6c193e49625d6122943beca49b2d6b501439
George Magdy <georgtriple@gmail.com>
2022-05-31 03:41:42 +0200

    Add custom error handler

---
+++ Task-Manager-API/middleware/error-handler.js
@@ -0,0 +1,10 @@
+const errorHandlerMiddleware = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Something went wrong';
+
+  res.status(statusCode).json({
+    message,
+  });
+};
+
+module.exports = errorHandlerMiddleware;
