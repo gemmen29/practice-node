8574161c0be732f589d1d1b43101f6efd4d3ae34
George Magdy <georgtriple@gmail.com>
2022-05-31 16:23:46 +0200

    Add pagination functionality

--- Store-API/controllers/products.js
+++ Store-API/controllers/products.js
@@ -35,6 +35,11 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(',').join(' ');
     result = result.select(fieldsList);
   }
+  //Pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
 
   const products = await result;
   res.status(200).json({ nbHits: products.length, products });
