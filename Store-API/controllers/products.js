72a3133dbb3691e1b0e0436ca3cfe8ed100ae4e0
George Magdy <georgtriple@gmail.com>
2022-05-31 08:33:03 +0200

    Add search by featured parameter

--- Store-API/controllers/products.js
+++ Store-API/controllers/products.js
@@ -4,13 +4,19 @@ const getAllProductsTesting = async (req, res) => {
   const products = await Product.find({
     featured: true,
   });
-  console.log(products);
   res.status(200).json({ nbHits: products.length, products });
 };
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find({});
-  console.log(products);
+  const { featured } = req.query;
+  console.log(featured);
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === 'true';
+  }
+
+  const products = await Product.find(queryObject);
   res.status(200).json({ nbHits: products.length, products });
 };
 
