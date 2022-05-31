0a4a6872526c1d4e65d311923b0372b61bfa35ff
George Magdy <georgtriple@gmail.com>
2022-05-31 03:42:03 +0200

    Use custom error handler in app

--- Task-Manager-API/app.js
+++ Task-Manager-API/app.js
@@ -4,6 +4,7 @@ const tasksRouter = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
 const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // middleware
 app.use(express.json());
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use('/api/v1/tasks', tasksRouter);
 
 app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
