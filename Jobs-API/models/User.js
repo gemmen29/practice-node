efc61a8ade12db90ec1782fa0e13b15165ab6738
George Magdy <georgtriple@gmail.com>
2022-07-25 06:10:08 +0200

    Add User model

---
+++ Jobs-API/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    minlength: [3, 'Name must be at least 3 characters long'],
+    maxlength: [50, 'Name must be at most 50 characters long'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please provide a valid email',
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [6, 'Password must be at least 6 characters long'],
+    maxlength: [12, 'Password must be at most 12 characters long'],
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema);
