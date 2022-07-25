2e60e6430e7de41683416dbd21bd111e0b70faaf
George Magdy <georgtriple@gmail.com>
2022-07-25 18:45:01 +0200

    Add job models

---
+++ Jobs-API/models/Job.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const JobSchema = mongoose.Schema({
+  company: {
+    type: String,
+    required: [true, 'Please provide a company name'],
+    maxlength: [50, 'Company name must be at most 50 characters long'],
+  },
+  position: {
+    type: String,
+    required: [true, 'Please provide a position'],
+    maxlength: [100, 'Position must be at most 100 characters long'],
+  },
+  status: {
+    type: String,
+    enum: ['interview', 'declined', 'pending', 'hired'],
+    default: 'pending',
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'Please provide a user'],
+  },
+});
