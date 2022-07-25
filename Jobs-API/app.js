6b71ab45589dea481108fdae69df6f6b27df5b4a
George Magdy <georgtriple@gmail.com>
2022-07-25 05:23:29 +0200

    Connect to DB

--- Jobs-API/app.js
+++ Jobs-API/app.js
@@ -3,6 +3,8 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+// connectDB
+const connectDB = require('./db/connect');
 //routers
 const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
@@ -24,6 +26,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () => {
       console.log(`Server is listening port ${port}...`);
     });
